eb91161817fb2affc4d49b40e13e551c3c9e37d0
magrid <magridprisca@gmail.com>
2021-06-16 21:04:33 +0700

    fungsi tambah dan hapus user

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -33,8 +33,50 @@ class Admin extends CI_Controller {
     }
     
     public function add_user(){
-        
         $this->load->view('tambah_user');
+    }
+    
+    public function add_process(){
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|max_length[15]|is_unique[tb_user.username]');
+		$this->form_validation->set_rules('email', 'Email', 'required|is_unique[tb_user.email]');
+		$this->form_validation->set_rules('password', 'Password', 'required|trim');
+		// $this->form_validation->set_rules('level', 'Level', 'required');
+		// $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password]');
+        if ($this->form_validation->run() == FALSE) {
+			$errors = $this->form_validation->error_array();
+			// $this->session->set_flashdata('errors', $errors);
+			// $this->session->set_flashdata('input', $this->input->post());
+			// redirect('Admin/add_user');
+            echo '<script>alert("Gagal menyimpan data!");window.location.href="'.site_url('/Admin/add_user').'";</script>';
+
+		} else {
+			$username = $this->input->post('username');
+			$nama = $this->input->post('nama');
+			$email = $this->input->post('email');
+			$password = $this->input->post('password');
+			$level = $this->input->post('level');
+			$pass = password_hash($password, PASSWORD_DEFAULT);
+			$data = [
+				'username' => $username,
+				'nama_lengkap' => $nama,
+				'email' => $email,
+				'password' => $pass,
+				'level'	=> $level
+			];
+			$insert = $this->user_model->create($data);
+			if($insert){
+                redirect('Admin/view_user');
+				// echo '<script>alert("Sukses! Anda berhasil melakukan register. Silahkan login untuk mengakses data.");window.location.href="'.site_url('/Admin/view_user').'";</script>';
+			}else{
+                echo '<script>alert("Sukses! Anda berhasil melakukan register. Silahkan login untuk mengakses data.");window.location.href="'.site_url('/Admin/add_user').'";</script>';
+            }
+		}
+    }
+
+    function del_user($username){
+        $insert = $this->user_model->delete($username);
+        redirect('Admin/view_user');
 
     }
 
